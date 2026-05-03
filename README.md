b19be986832a41330c322d7d494c929ef1f33d64
Detroix23 <detroix23@gmail.com>
2026-05-03 19:47:35 +0200

    Implemented basic movement and camera.

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 Game made with `pyxel`, training the _Nuit du C0de_.
 
 **Costraints**.
+- Python version 3.11.
 - Write everything in one single file.
 - One single `.pyxres` file.
 - Docstrings in French.
+- Must use a tilemap, a music track, the `pyxel` camera. 
---
+++ main.py
@@ -0,0 +1,283 @@
+"""
+# A-Steroid
+main.py
+"""
+
+from typing import Final
+
+import pyxel
+
+TRANSPARENCY: Final[int] = 8
+""" Clé de couleur pour la transparence. """
+
+
+class Vector2D:
+	"""
+	# `Vector2D` 2 `float`s (x; y)
+	"""
+	x: float
+	y: float
+
+	def __init__(self, x: float, y: float) -> None:
+		self.x = x
+		self.y = y
+
+	def __str__(self) -> str:
+		return f"({self.x}; {self.y})"
+
+	def __add__(self, other: object) -> 'Vector2D':
+		if isinstance(other, Vector2D):
+			return Vector2D(
+				self.x + other.x,
+				self.y + other.y,
+			)
+		else:
+			return NotImplemented
+	
+	def __sub__(self, other: object) -> 'Vector2D':
+		if isinstance(other, Vector2D):
+			return Vector2D(
+				self.x - other.x,
+				self.y - other.y,
+			)
+		else:
+			return NotImplemented
+
+	def __mul__(self, other: object) -> 'Vector2D':
+		if isinstance(other, float) or isinstance(other, int):
+			return Vector2D(
+				self.x * other,
+				self.y * other,
+			)
+		else:
+			return NotImplemented
+
+	def __truediv__(self, other: object) -> 'Vector2D':
+		if isinstance(other, float) or isinstance(other, int):
+			return Vector2D(
+				self.x / other,
+				self.y / other,
+			)
+		else:
+			return NotImplemented
+
+
+class Player:
+	"""
+	# `Player` principal: le vaisseau.
+	"""
+	SPRITE_SHIP_IMAGE: int = 0
+	SPRITE_SHIP_POSITION: tuple[int, int] = (0, 0)
+	SPRITE_SHIP_SIZE: tuple[int, int] = (16, 16)
+
+	parent_app: 'App'
+	position: Vector2D
+	inertia: Vector2D
+	acceleration: float
+	rotation: float
+	drag: float
+
+	def __init__(
+		self, 
+		parent_app: 'App',
+		start_position: Vector2D,
+		acceleration: float,
+		drag: float,
+	) -> None:
+		"""
+		Crée le `Player`, en renseignant son parent.
+		"""
+		self.parent_app = parent_app
+		self.position = start_position
+		self.inertia = Vector2D(0.0, 0.0)
+		self.acceleration = acceleration
+		self.drag = drag
+		self.rotation = 0.0
+
+	def update(self) -> None:
+		"""
+		Gère le déplacement et les actions du joueur.
+		"""
+		# Direction.
+		self.rotation = -pyxel.atan2(
+			self.position.x - pyxel.mouse_x, 
+			self.position.y - pyxel.mouse_y,
+		)
+
+		# Souris.
+		if pyxel.btn(pyxel.KEY_UP):
+			inertia: Vector2D = Vector2D(
+				pyxel.cos(self.rotation - 90.0),
+				pyxel.sin(self.rotation - 90.0),
+			) * self.acceleration
+			self.inertia += inertia
+		if pyxel.btn(pyxel.KEY_DOWN):
+			inertia: Vector2D = Vector2D(
+				pyxel.cos(self.rotation - 90.0),
+				pyxel.sin(self.rotation - 90.0),
+			) * (self.acceleration / -3.0)
+			self.inertia += inertia
+		if pyxel.btn(pyxel.KEY_LEFT):
+			inertia: Vector2D = Vector2D(
+				pyxel.cos(self.rotation),
+				pyxel.sin(self.rotation),
+			)
+			centripete: Vector2D = Vector2D(
+				pyxel.cos(self.rotation - 90.0),
+				pyxel.sin(self.rotation - 90.0),
+			)
+			lateral: Vector2D = (centripete + inertia) * (self.acceleration / 2.0)
+		
+			self.inertia += lateral
+		if pyxel.btn(pyxel.KEY_RIGHT):
+			inertia: Vector2D = Vector2D(
+				pyxel.cos(self.rotation),
+				pyxel.sin(self.rotation),
+			)
+			centripete: Vector2D = Vector2D(
+				pyxel.cos(self.rotation - 90.0),
+				pyxel.sin(self.rotation - 90.0),
+			)
+			lateral: Vector2D = (centripete - inertia) * (self.acceleration / 2.0)
+
+			self.inertia += lateral
+
+		# Position.
+		self.position += self.inertia
+		self.inertia /= self.drag
+
+
+	def draw(self) -> None:
+		"""
+		Dessine le vaisseau du joueur.
+		"""
+		pyxel.blt(
+			self.position.x - Player.SPRITE_SHIP_SIZE[0] / 2,
+			self.position.y - Player.SPRITE_SHIP_SIZE[1] / 2,
+			Player.SPRITE_SHIP_IMAGE,
+			Player.SPRITE_SHIP_POSITION[0],
+			Player.SPRITE_SHIP_POSITION[1],
+			Player.SPRITE_SHIP_SIZE[0],
+			Player.SPRITE_SHIP_SIZE[1],
+			colkey=TRANSPARENCY,
+			rotate=self.rotation,
+			scale=1.0
+		)
+
+
+class Ui:
+	"""
+	# Interface `Ui` du jeu.
+	"""
+	SPRITE_MOUSE_IMAGE: int = 0
+	SPRITE_MOUSE_POSITION: tuple[int, int] = (0, 16)
+	SPRITE_MOUSE_SIZE: tuple[int, int] = (16, 16)
+
+	parent_app: 'App'
+
+	def __init__(self, parent_app: 'App') -> None:
+		"""
+		Instancie la sous-class `Ui`.
+		"""
+		self.parent_app = parent_app
+
+	def update(self) -> None:
+		"""
+		Mets à jour la camera.
+		"""
+		pyxel.camera(
+			self.parent_app.player.inertia.x,
+			self.parent_app.player.inertia.y,
+		)	
+
+	def draw(self) -> None:
+		"""
+		Dessine toute l'interface: souris, textes.
+		"""
+		# Décors
+		frame_margin = 5
+		pyxel.rectb(
+			x=frame_margin,
+			y=frame_margin,
+			w=pyxel.width - 2 * frame_margin,
+			h=pyxel.height - 2 * frame_margin,
+			col=pyxel.COLOR_WHITE,
+		)
+
+		# Souris animée.
+		mouse_frame: int = abs(pyxel.floor(pyxel.frame_count / 4) % 7 - 3)
+		pyxel.blt(
+			pyxel.mouse_x - Ui.SPRITE_MOUSE_SIZE[0] // 2,
+			pyxel.mouse_y - Ui.SPRITE_MOUSE_SIZE[1] // 2,
+			Ui.SPRITE_MOUSE_IMAGE,
+			Ui.SPRITE_MOUSE_POSITION[0] + mouse_frame * 16,
+			Ui.SPRITE_MOUSE_POSITION[1],
+			Ui.SPRITE_MOUSE_SIZE[0],
+			Ui.SPRITE_MOUSE_SIZE[1],
+			colkey=TRANSPARENCY,
+		)
+
+
+class App:
+	"""
+	# A-Steroid cadre principal d'execution `App`. 
+	Englobe l'entiereté du jeu. 
+	"""
+	
+	score: int
+	ui: Ui
+	player: Player
+
+	def __init__(self) -> None:
+		"""
+		Crée l'`App`, le context `pyxel`, et lance la boucle.
+		"""
+		pyxel.init(
+			width=256,
+			height=256,
+			title="A-Steroid",
+			fps=30,
+			quit_key=pyxel.KEY_ESCAPE,
+		)
+
+		self.score = 0
+		self.ui = Ui(parent_app=self)
+		self.player = Player(
+			parent_app=self,
+			start_position=Vector2D(pyxel.width / 2, pyxel.height / 2),
+			acceleration=1.2,
+			drag=1.1,
+		)
+
+		pyxel.camera()
+		pyxel.load("theme.pyxres")
+
+		print("(?) App() Lancage du jeu...")
+		pyxel.run(self.update, self.draw)
+
+	def update(self) -> None:
+		"""
+		Gère l'état du jeu, écoute les entrés.
+		"""	
+		self.ui.update()
+		self.player.update()
+
+	def draw(self) -> None:
+		"""
+		Dessine, affiche avec `pyxel` le jeu.
+		"""
+		pyxel.cls(0)
+		self.player.draw()
+		self.ui.draw()
+
+
+
+def main() -> None:
+	"""
+	Point d'entré principal.
+	"""
+	print("\n# A-Steroid.\n")
+	_app = App()
+
+if __name__ == "__main__":
+	main()
